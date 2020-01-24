6a1870478cebad73785c15eddf92246b9a45b9cf
Fedor Chervyakov <fchervyakov@gmail.com>
2020-01-25 01:54:50 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Rabbimq JavaScript tutorial
+(using amqp.node client)
+Tutorial from <https://www.rabbitmq.com/tutorials/>
+
+### Sercer credentials
+Credentials are stored in top directory in a __.secret.json__ file.
+File format is as following:
+```
+{
+    "user": "someuser",
+    "pass": "some password",
+    "ip":   "10.0.0.1"
+}
+```
---
+++ tut1/receive.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const amqp = require('amqplib/callback_api');
+const secret = require('../.secret.json');
+
+const opt = { credentials: require('amqplib').credentials.plain(secret.user, secret.pass) };
+
+amqp.connect(`amqp://${secret.ip}`, opt, function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function(msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+        }, {
+            noAck: true
+        });
+    });
+});
